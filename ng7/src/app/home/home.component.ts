6e149f65aeb0daba5a9f3be84da0b6ceb1a935c0
DPesheva <dpessheva@gmail.com>
2019-07-28 14:36:02 +0200

    update-home component

--- ng7/src/app/home/home.component.ts
+++ ng7/src/app/home/home.component.ts
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   // tslint:disable-next-line: no-inferrable-types
-  h1Style: boolean = false;
+  // h1Style: boolean = false;
+
+  users: {};
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
   }
 
-  firstClick() {
-    this.data.firstClick();
-  }
+
 }
